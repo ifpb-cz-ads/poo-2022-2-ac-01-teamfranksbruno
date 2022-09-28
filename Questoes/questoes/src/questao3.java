e3f6be5a087d18da43c22dad3851688f23ac85eb
aaFranks <maciel.franks@academico.ifpb.edu.br>
2022-09-28 16:06:00 -0300

    Adicinando os códigos das questões 3 e 6.

---
+++ Questoes/questoes/src/questao3.java
@@ -0,0 +1,5 @@
+public class questao3 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Terminei a primeira aula com um programa Java!");
+    }
+}
---
+++ Questoes/questoes/src/questao6.java
@@ -0,0 +1,6 @@
+public class questao6 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Franks");
+        System.out.println("Nenhum time");
+    }
+}
