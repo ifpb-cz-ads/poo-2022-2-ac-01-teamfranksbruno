6c5f53e388ae63fa852642c8ef47fdbcb1e6fea2
aaFranks <105517926+aaFranks@users.noreply.github.com>
2022-10-05 20:56:10 -0300

    Deixando só os dois arquivos

--- atividade/atividade/src/App.java
+++
@@ -1,9 +0,0 @@
-package atividade.atividade.src;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        String msn = "Ola, mundo!";
-
-        System.out.println(msn);
-    }
-}
